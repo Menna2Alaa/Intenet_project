43b2d173d13df789cfd8586c33073afb0163a35e
Menna2Alaa <120278348+Menna2Alaa@users.noreply.github.com>
2023-06-25 13:30:33 +0300

    Initial commit

--- app/src/main/java/com/example/intenetproject/MainActivity.java
+++ app/src/main/java/com/example/intenetproject/MainActivity.java
@@ -16,27 +16,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        EditText Name ;
-        EditText phoneNumber ;
         Button button ;
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
-        phoneNumber =(EditText) findViewById(R.id.textView);
-        Name=(EditText) findViewById(R.id.textView1);
         button=findViewById(R.id.add_contact_button);
 
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(ContactsContract.Intents.Insert.ACTION);
-                intent.setType(ContactsContract.RawContacts.CONTENT_TYPE);
-                intent.putExtra(ContactsContract.Intents.Insert.PHONE, phoneNumber.getText());
-                intent.putExtra(ContactsContract.Intents.Insert.NAME, Name.getText());
-                startActivity(intent);
+                Intent i = new Intent(getApplicationContext(), MainActivity2.class);
+                startActivity(i);
             }
         });
     }
--- app/src/main/java/com/example/intenetproject/MainActivity2.java
+++ app/src/main/java/com/example/intenetproject/MainActivity2.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,15 +15,23 @@ public class MainActivity2 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
+        EditText Name ;
+        EditText phoneNumber ;
+
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
+        phoneNumber =(EditText) findViewById(R.id.textView);
+        Name=(EditText) findViewById(R.id.textView1);
 
-    }
-    public void homeScreen(View view) {
-        Intent i = new Intent(getApplicationContext(), MainActivity.class);
-        startActivity(i);
+
+        Intent intent = new Intent(ContactsContract.Intents.Insert.ACTION);
+        intent.setType(ContactsContract.RawContacts.CONTENT_TYPE);
+        intent.putExtra(ContactsContract.Intents.Insert.PHONE, phoneNumber.getText());
+        intent.putExtra(ContactsContract.Intents.Insert.NAME, Name.getText());
+        startActivity(intent);
+
         Toast.makeText(this, "great", Toast.LENGTH_SHORT).show();
     }
 }
