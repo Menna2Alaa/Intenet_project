bb5005680c0fa29923e12fc09fa342b31b1cc6c5
Menna2Alaa <120278348+Menna2Alaa@users.noreply.github.com>
2023-06-25 14:05:12 +0300

    Initial commit

--- app/src/main/java/com/example/intenetproject/MainActivity.java
+++ app/src/main/java/com/example/intenetproject/MainActivity.java
@@ -4,23 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        Button button ;
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         button=findViewById(R.id.add_contact_button);
 
-
+        onClicks();
+    }
+    public void onClicks()
+    {
         button.setOnClickListener(view -> {
             Intent i = new Intent(getApplicationContext(), MainActivity2.class);
             startActivity(i);
