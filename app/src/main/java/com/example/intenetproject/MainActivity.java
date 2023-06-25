7a9dea2b8334f81f3999a471383aafce4bb2187c
Menna2Alaa <120278348+Menna2Alaa@users.noreply.github.com>
2023-06-25 13:31:54 +0300

    Initial commit

--- app/src/main/java/com/example/intenetproject/MainActivity.java
+++ app/src/main/java/com/example/intenetproject/MainActivity.java
@@ -3,13 +3,9 @@ package com.example.intenetproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,12 +21,9 @@ public class MainActivity extends AppCompatActivity {
         button=findViewById(R.id.add_contact_button);
 
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(getApplicationContext(), MainActivity2.class);
-                startActivity(i);
-            }
+        button.setOnClickListener(view -> {
+            Intent i = new Intent(getApplicationContext(), MainActivity2.class);
+            startActivity(i);
         });
     }
 }
