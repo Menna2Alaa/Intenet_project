8144278345a1e14e4719f9d28a35f2e5c7f5b58f
Menna2Alaa <120278348+Menna2Alaa@users.noreply.github.com>
2023-06-25 13:35:17 +0300

    Initial commit

--- app/src/main/java/com/example/intenetproject/MainActivity2.java
+++ app/src/main/java/com/example/intenetproject/MainActivity2.java
@@ -13,7 +13,7 @@ public class MainActivity2 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        EditText Name ;
+        EditText name ;
         EditText phoneNumber ;
 
 
@@ -21,13 +21,13 @@ public class MainActivity2 extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
 
         phoneNumber =(EditText) findViewById(R.id.textView);
-        Name=(EditText) findViewById(R.id.textView1);
+        name=(EditText) findViewById(R.id.textView1);
 
 
         Intent intent = new Intent(ContactsContract.Intents.Insert.ACTION);
         intent.setType(ContactsContract.RawContacts.CONTENT_TYPE);
         intent.putExtra(ContactsContract.Intents.Insert.PHONE, phoneNumber.getText());
-        intent.putExtra(ContactsContract.Intents.Insert.NAME, Name.getText());
+        intent.putExtra(ContactsContract.Intents.Insert.NAME, name.getText());
         startActivity(intent);
 
         Toast.makeText(this, "great", Toast.LENGTH_SHORT).show();
